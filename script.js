5a11c37c98cf4ffe3662d458b7256e0952822799
s-afrid <afrid0052@gmail.com>
2025-08-14 14:30:25 +0530

    dividing the amount

--- script.js
+++ script.js
@@ -15,16 +15,16 @@ function getSplits(n,totalBill,nhigh,nmed,nlow){
     }
     // if any one of them is zero
     else if(nhigh==0) {
-        mp = nlow/n;
-        lp = nmed/n;
+        mp = 0.9;
+        lp = 0.1;
     }
     else if(nmed==0) {
-        hp = nlow/n;
-        lp = nhigh/n;
+        hp = 0.9;
+        lp = 0.1;
     }
     else if(nlow==0) {
-        hp = nmed/n;
-        mp = nhigh/n;
+        hp = 0.9;
+        mp = 0.1;
     }
     else {
         hp = 0.6;
@@ -53,35 +53,28 @@ function getSplits(n,totalBill,nhigh,nmed,nlow){
         contributions.push(parseFloat((lowContribution/nlow).toFixed(2)));
         
     }
+
+    // let contributions = [contributionsh,contributionsm,contributionsl]
     
-    if ((nhigh==0) && (nmed>=nlow)){
-        contributions.reverse()
-    }
-    else if ((nmed==0) && (nhigh>=nmed)){
-        contributions.reverse()
-    }
-    else if ((nlow==0) && (nhigh>=nmed)){
-        contributions.reverse()
-    }
+    // if ((nhigh==0) && (nmed>=nlow)){
+    //     contributions.reverse()
+    // }
+    // else if ((nmed==0) && (nhigh>=nlow)){
+    //     contributions.reverse()
+    // }
+    // else if ((nlow==0) && (nhigh>=nmed)){
+    //     contributions.reverse()
+    // }
 
 
 
     console.log(contributions);
-    (() => {
-        sum = 0;
-        for(let i=0;i<contributions.length;i++){
-            sum = sum + contributions[i]
-        }
-        console.log(parseFloat(sum.toFixed(2)))
-    })();
-    console.log()
-    
 }
 
 let totalBill = 200;
 let n = 10;
-let nhigh = 6;
+let nhigh = 8;
 let nmed = 2;
-let nlow = 2;
+let nlow = 0;
 
 getSplits(n,totalBill,nhigh,nmed,nlow)
